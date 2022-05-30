b9e9b684b8822f7911f8725a2024612468d7c174
Kacper <kacperjanzieba@gmail.com>
2022-05-30 21:43:36 +0200

    Add getting average color of the screen functionality

--- screen_capture.py
+++ screen_capture.py
@@ -1,1 +1,65 @@
-#TODO
+import numpy as np
+import mss
+
+
+# this function captures the screen and converts it to numpy array
+def capture_screen():
+    # get screen capture with mss
+    with mss.mss() as sct:
+        # get the screen
+        ss = sct.grab(sct.monitors[1])
+        img_np = np.array(ss)
+    # lower the resolution
+    img_np = img_np[::10, ::10, :]
+    return img_np
+
+
+# this function extracts the borders from the screen
+def extract_borders(img_np):
+    border_l = img_np[3:-3, :3, :]
+    border_r = img_np[3:-3, -3:, :]
+    border_t = img_np[:3, :, :]
+    border_b = img_np[-3:, :, :]
+    return border_l, border_r, border_t, border_b
+
+
+# this function divides the borders into n parts
+def divide_borders(img_np_borders, n):
+    img_np_borders_divided = np.split(img_np_borders, n, axis=2)
+    # convert to numpy array
+    img_np_borders_divided = np.array(img_np_borders_divided)
+    return img_np_borders_divided
+
+
+# this function gets the average color of a numpy array
+def get_average_color_x(img_np, n):
+    img_np_average_color = []
+    size = img_np.shape[1]
+    for i in range(1, n + 1):
+        img_np_average_color.append(np.average(img_np[:, ((size // n) * (i - 1)):((size // n) * i), :], axis=(0, 1)))
+    return img_np_average_color
+
+
+def get_average_color_y(img_np, n):
+    img_np_average_color = []
+    size = img_np.shape[0]
+    for i in range(1, n + 1):
+        img_np_average_color.append(
+            np.average(img_np[((size // n) * (i - 1)):((size // n) * i), :, :], axis=(0, 1)))
+    return img_np_average_color
+
+
+def get_screen_data():
+    img_np = capture_screen()
+    borders = extract_borders(img_np)
+
+    # number of LED diodes
+    n = 20
+
+    img_np_borders_average_color_l = get_average_color_y(borders[0], n)
+    img_np_borders_average_color_r = get_average_color_y(borders[1], n)
+    img_np_borders_average_color_t = get_average_color_x(borders[2], n)
+    img_np_borders_average_color_b = get_average_color_x(borders[3], n)
+
+    return (img_np_borders_average_color_l, img_np_borders_average_color_r, img_np_borders_average_color_t,
+            img_np_borders_average_color_b)
