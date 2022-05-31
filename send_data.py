a224ea64556d2beee1af8d1d4e04ddbed4501167
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-05-31 17:49:19 +0200

    add send_data.py

---
+++ send_data.py
@@ -0,0 +1,30 @@
+import serial
+import numpy as np
+import screen_capture
+
+LEFT = 0
+RIGHT = 1
+TOP = 2
+
+
+def wait_until_received(arduino):
+    arduino.read()
+    arduino.reset_input_buffer()
+
+
+def start():
+    arduino = serial.Serial(port='COM3', baudrate=19200, timeout=1)
+
+    while True:
+        data = np.array(screen_capture.get_screen_data(), dtype=int)
+
+        for d in data[LEFT]:
+            arduino.write(bytes([d[0], d[1], d[2]]))
+
+        for d in data[TOP]:
+            arduino.write(bytes([d[0], d[1], d[2]]))
+
+        for d in data[RIGHT]:
+            arduino.write(bytes([d[0], d[1], d[2]]))
+
+        wait_until_received(arduino)
